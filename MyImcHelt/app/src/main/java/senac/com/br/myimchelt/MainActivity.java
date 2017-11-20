2fb569c7fbf69d0595ed2d5900168d30d2e661ae
heltonmatiazi <heltonmatiazi@gmail.com>
2017-11-20 16:22:14 -0200

    finalizado o calculo de IMC, progressbar funcional, shared prefs funcionando

--- MyImcHelt/app/src/main/java/senac/com/br/myimchelt/MainActivity.java
+++ MyImcHelt/app/src/main/java/senac/com/br/myimchelt/MainActivity.java
@@ -3,101 +3,125 @@ package senac.com.br.myimchelt;
 import android.app.Activity;
 import android.content.ContentValues;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.media.ThumbnailUtils;
 import android.net.Uri;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.StrictMode;
+import android.preference.PreferenceManager;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static senac.com.br.myimchelt.R.style.textView;
+
 
 public class MainActivity extends AppCompatActivity {
-    static final int PICTURE_RESULT = 1;
-    String mCurrentPhotoPath;
-    ContentValues values;
-    private Uri file;
-    private ImageView userImage;
-    Bitmap help1;
-    ThumbnailUtils thumbnail;
+
+   private ImageView userImage;
+   private final int requestCode = 20;
+   private EditText pesoEt;
+   private EditText alturaEt;
+   private ProgressBar mProgressBar;
+   private int progressStatus = 0;
+   private TextView imcTexto;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         userImage = (ImageView) findViewById(R.id.userImage);
-        values = new ContentValues();
+        pesoEt = (EditText) findViewById(R.id.peso);
+        alturaEt = (EditText) findViewById(R.id.altura);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+        mProgressBar.setVisibility(View.INVISIBLE);
+        imcTexto = (TextView) findViewById(R.id.imcTexto);
+        // precisa disso pra poder rodar a camera no sandbox do emulador
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
+
+
 
     }
 
     public void novaImagem(View v) {
-        // intent que chama a camera
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // pegando a URI do arquivo
-        file = Uri.fromFile(getFile());
-
-        // adicionando a URI ao intent
-        intent.putExtra(MediaStore.EXTRA_OUTPUT,file);
-
-        if(intent.resolveActivity(getPackageManager())!=null)
-        {
-            startActivityForResult(intent, PICTURE_RESULT);
-        }
-    }
-    // criando e retornando o caminho para a imagem
-    private File getFile() {
-        File folder = Environment.getExternalStoragePublicDirectory("/From_camera/imagens");// file path
-
-        // se o folder não existir, ele será criado
-        // pq aparentemente é uma ideia boa pra caralho rodar shell no meio do java
-        if(!folder.exists())
-        {folder.mkdir();}
-
-        // gerando nomes únicos para as imagens
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_"+ timeStamp + "_";
-        File image_file = null;
-        // criando o arquivo com o nome gerado anteriormente
-        try {
-            image_file = File.createTempFile(imageFileName,".jpg",folder);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        // setando o caminho absoluto das imagens
-        mCurrentPhotoPath = image_file.getAbsolutePath();
-        return image_file;
+        Intent photoCaptureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(photoCaptureIntent, requestCode);
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode == PICTURE_RESULT) {
-            if(resultCode == Activity.RESULT_OK) {
-                try {
-                    help1 = MediaStore.Images.Media.getBitmap(getContentResolver(),file);
-                    userImage.setImageBitmap( thumbnail.extractThumbnail(help1,help1.getWidth(),help1.getHeight()));
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-            }
+        super.onActivityResult(requestCode, resultCode, data);
+        if(this.requestCode == requestCode && resultCode == RESULT_OK){
+            Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+            userImage.setImageBitmap(bitmap);
+
         }
     }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+
     @Override
     protected void onPause() {
-
         super.onPause();
     }
 
     public void calcularImc(View v) {
+        SharedPreferences getPrefs = PreferenceManager
+                .getDefaultSharedPreferences(getBaseContext());
+        float peso = getPrefs.getFloat("peso", 0);
+        float altura = getPrefs.getFloat("altura",0);
+        float imc = getPrefs.getFloat("imc",0);
+
+
+        float pesoFornecido = Float.parseFloat(pesoEt.getText().toString());
+        float alturaFornecida = Float.parseFloat(alturaEt.getText().toString());
+        if(pesoFornecido <= 1 || alturaFornecida <= 1){
+            Toast.makeText(this, "O peso ou a altura são inválidos", Toast.LENGTH_SHORT).show();
+        }
+        float imcFinal;
+        imcFinal = (pesoFornecido/(alturaFornecida*alturaFornecida));
+
+        Toast.makeText(this,"imcfinal: "+imcFinal,Toast.LENGTH_SHORT).show();
+
+        int maxValue=mProgressBar.getMax();
+
+
+        if(imcFinal <= 15){
+            mProgressBar.setProgress(1);
+        }else if(imcFinal > 15 && imcFinal <30){
+           mProgressBar.setProgress(50);
+            //delayedUpdate(imcFinal);
+        }else{
+           mProgressBar.setProgress(maxValue);
+        }
+           mProgressBar.setVisibility(View.VISIBLE);
+        imcTexto.setText("Seu imc é: "+imcFinal);
+
+        SharedPreferences.Editor editor = getPrefs.edit();
+        editor.putFloat("peso",pesoFornecido);
+        editor.putFloat("altura",alturaFornecida);
+        editor.putFloat("imc",imcFinal);
+        editor.apply();
     }
 
     public void consultarAcademias(View v) {
         Intent i = new Intent(this, PesquisarAcademia.class);
         startActivity(i);
     }
+
 
 
 }
